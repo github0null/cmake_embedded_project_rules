2fd14af0d297f09e01174241407f69759ce0bebd
github0null <2584456014@qq.com>
2022-02-21 18:06:06 +0800

    update

--- kconfig/kconfig.cmake
+++ kconfig/kconfig.cmake
@@ -1,17 +1,20 @@
 
-function(kconfig_include file_path)
+function(kconfig_include CONFIG_FILE)
 
     set(options)
-    set(oneValueArgs CONFIG_FILE DIRECTORY)
+    set(oneValueArgs OUTPUT_DIRECTORY)
     set(multiValueArgs)
     cmake_parse_arguments(_ "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
-    if("${__CONFIG_FILE}_" STREQUAL "_")
-        file(READ "${CMAKE_CURRENT_LIST_DIR}/.config" _kconfig_cont)
-    else()
-        file(READ "${__CONFIG_FILE}" _kconfig_cont)
+    if("${CONFIG_FILE}_" STREQUAL "_")
+        set(CONFIG_FILE "${CMAKE_CURRENT_LIST_DIR}/.config")
     endif()
 
+    if("${__OUTPUT_DIRECTORY}_" STREQUAL "_")
+        set(__OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
+    endif()
+
+    file(READ "${CONFIG_FILE}" _kconfig_cont)
     
 
 endfunction()
