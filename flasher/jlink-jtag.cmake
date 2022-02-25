b82a48463788100cad6eff66a44ba2b7d4cedfc9
github0null <2584456014@qq.com>
2022-02-25 11:39:57 +0800

    update flasher log

--- flasher/jlink-jtag.cmake
+++ flasher/jlink-jtag.cmake
@@ -10,14 +10,14 @@ function(output_flash_command target_name)
     
     if(CMAKE_HOST_WIN32)
         add_custom_command(TARGET "${target_name}" POST_BUILD
-            COMMAND ${CMAKE_COMMAND} -E echo "output '${FLASHER_NAME}' flasher command for '${FLASHER_MCPU_NAME}' ..."
+            COMMAND ${CMAKE_COMMAND} -E echo "Output '${FLASHER_NAME}' flasher command for '${FLASHER_MCPU_NAME}' ..."
             COMMAND ${CMAKE_COMMAND} -E echo "@echo off" > flash.bat
             COMMAND ${CMAKE_COMMAND} -E echo "jlink -ExitOnError 1 -AutoConnect 1 -Device ${FLASHER_MCPU_NAME} -If JTAG -Speed 8000 -CommandFile \"${CMD_FILE_PATH}\" -JTAGConf -1,-1" >> flash.bat
             WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
             VERBATIM)
     else()
         add_custom_command(TARGET "${target_name}" POST_BUILD
-            COMMAND ${CMAKE_COMMAND} -E echo "output '${FLASHER_NAME}' flasher command for '${FLASHER_MCPU_NAME}' ..."
+            COMMAND ${CMAKE_COMMAND} -E echo "Output '${FLASHER_NAME}' flasher command for '${FLASHER_MCPU_NAME}' ..."
             COMMAND ${CMAKE_COMMAND} -E echo "jlink -ExitOnError 1 -AutoConnect 1 -Device ${FLASHER_MCPU_NAME} -If JTAG -Speed 8000 -CommandFile \"${CMD_FILE_PATH}\" -JTAGConf -1,-1" > flash.sh
             WORKING_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
             VERBATIM)
